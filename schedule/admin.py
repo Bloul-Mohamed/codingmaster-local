6599cb138c0aa58caf76ae2a897e08ec8674e030
Mohamed-Ridha <medbloul2002@gmail.com>
2025-04-07 11:00:10 +0100

    create page admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Stadium, Department, Schedule, checks
+
+
+@admin.register(Stadium)
+class StadiumAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'location', 'capacity', 'is_active')
+    search_fields = ('name',)
+    ordering = ('-id',)
+    list_per_page = 10
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'location', 'capacity', 'is_active')
+        }),
+    )
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    ordering = ('-id',)
+    list_per_page = 10
+    fieldsets = (
+        (None, {
+            'fields': ('name',)
+        }),
+    )
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'department', 'date',
+                    'start_time', 'end_time', 'is_active')
+    search_fields = ('department__name',)
+    ordering = ('-id',)
+    list_per_page = 10
+    fieldsets = (
+        (None, {
+            'fields': ('department', 'date', 'start_time', 'end_time', 'is_active')
+        }),
+    )
+
+
+@admin.register(checks)
+class ChecksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'counter', 'depertment', 'stadium')
+    search_fields = ('depertment__name',)
+    ordering = ('-id',)
+    list_per_page = 10
+    fieldsets = (
+        (None, {
+            'fields': ('counter', 'depertment', 'stadium')
+        }),
+    )
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email',
+                    'first_name', 'last_name', 'depertment')
+    search_fields = ('username', 'email')
+    ordering = ('-id',)
+    list_per_page = 10
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'first_name', 'last_name', 'depertment')
+        }),
+    )
